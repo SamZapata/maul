c4380c45856c4d605232357cf5388eedce3ffd81
samzapata <legionario.zapata@gmail.com>
2017-08-22 12:12:39 -0500

    update con restricciones de inicio de sesion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters    
 		#devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache) }
-    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:username, :password, :password_confirmation, :current_password, :avatar)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:username, :password, :password_confirmation, :current_password, :first_name, :last_name, :avatar)}
 	end
 
   #Con esta intrucción el controlador restringe el acceso hasta que el usuairo
